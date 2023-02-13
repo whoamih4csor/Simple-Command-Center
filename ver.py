f0b5ecf3dac8596acc9dfa7cb87dcf49e3722ef1
whoamih4csor <122777930+whoamih4csor@users.noreply.github.com>
2023-02-13 00:50:00 -0400

    add requirements.txt

---
+++ ver.py
@@ -0,0 +1,12 @@
+import pyaudio
+import cv2
+import psutil
+import numpy
+import customtkinter
+
+#test versions
+print( pyaudio.__name__ + ' ' + pyaudio.__version__)
+print( cv2.__name__ + ' ' + cv2.__version__)
+print( psutil.__name__ + ' ' + psutil.__version__)
+print( numpy.__name__ + ' ' + numpy.__version__)
+print( customtkinter.__name__ + ' ' + customtkinter.__version__)
